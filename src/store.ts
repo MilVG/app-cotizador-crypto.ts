3f6940ad055e29434dfcc41a7263f55d470217e5
notlim_dev <elmotigre6@gmail.com>
2024-10-03 14:47:20 -0500

    0018: Icorporación devtools para zustand
    Se envolvió la función devtools  a todos los states y funcionaes
    definidas dentro de la funcion create de zustand.

--- src/store.ts
+++ src/store.ts
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 import { CryptoCurrenciesResponseSchema } from "./schema/crypto-schema";
 import { CryptoCurrency } from "./types";
-
+import { devtools } from "zustand/middleware";
 type CryptoStore = {
   cryptocurrencies: CryptoCurrency[]
   fetchCryptos: () => Promise<void>
@@ -12,18 +12,21 @@ async function getCryptos() {
   const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
   const { data: { Data } } = await axios(url)
   const result = CryptoCurrenciesResponseSchema.safeParse(Data)
-
   if (result.success) {
     return result.data
   }
 
 }
-export const useCriptoStore = create<CryptoStore>((set) => ({
-  cryptocurrencies: [],
-  fetchCryptos: async () => {
-    const cryptocurrencies = await getCryptos()
-    set(() => ({
-      cryptocurrencies
-    }))
-  }
-}))
+export const useCriptoStore = create<CryptoStore>()(
+  devtools<CryptoStore>(
+    (set) => ({
+      cryptocurrencies: [],
+      fetchCryptos: async () => {
+        const cryptocurrencies = await getCryptos()
+        set(() => ({
+          cryptocurrencies
+        }))
+      },
+    }),
+  ),
+)
