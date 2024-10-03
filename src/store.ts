6c8c84fbb24c67d10922e68a10ce703e47d88711
notlim_dev <elmotigre6@gmail.com>
2024-10-03 13:35:58 -0500

    0015: Creación Tipado y State
    Se creó e implementó el tipado  para la función del hook useCryptoStore,
    función fetchCryptos y se agregó el state llamado cryptocurrencies,
    ademàs se agregaron los datos al state que se había creado.

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,13 @@
 import { create } from "zustand";
 import axios from "axios";
 import { CryptoCurrenciesResponseSchema } from "./schema/crypto-schema";
+import { CryptoCurrency } from "./types";
+
+type CryptoStore = {
+  cryptocurrencies: CryptoCurrency[]
+  fetchCryptos: () => Promise<void>
+}
+
 async function getCryptos() {
   const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
   const { data: { Data } } = await axios(url)
@@ -11,8 +18,12 @@ async function getCryptos() {
   }
 
 }
-export const useCriptoStore = create(() => ({
+export const useCriptoStore = create<CryptoStore>((set) => ({
+  cryptocurrencies: [],
   fetchCryptos: async () => {
     const cryptocurrencies = await getCryptos()
+    set(() => ({
+      cryptocurrencies
+    }))
   }
 }))
