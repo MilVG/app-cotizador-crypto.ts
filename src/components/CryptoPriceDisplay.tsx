46bcdfd1a2eb020bee11580021833b54a121d6d3
notlim_dev <elmotigre6@gmail.com>
2024-10-04 01:26:12 -0500

    0029: Estilos css Interfaz Datos API
    Se estilizó cada elemento de la interfaz de los datos de la api externa
    para una mejor relevancia.

--- src/components/CryptoPriceDisplay.tsx
+++ src/components/CryptoPriceDisplay.tsx
@@ -7,13 +7,21 @@ export default function CryptoPriceDisplay() {
   const comprobarResult = useMemo(() => Object.keys(result).length > 0, [result])
 
   return (
-    <div>
+    <div className="result-wrapper">
       {comprobarResult && (
         <>
           <h2>Cotizador</h2>
           <div className="result">
+            <img
+              src={`https://cryptocompare.com/${result.IMAGEURL}`}
+              alt="Imagen Cryptomoneda"
+            />
             <div>
               <p>El precio es de: <span>{result.PRICE}</span></p>
+              <p>Precio más alto del día: <span>{result.HIGHDAY}</span></p>
+              <p>Precio más bajo del día: <span>{result.LOWDAY}</span></p>
+              <p>Variación ultimas 24 Horas: <span>{result.CHANGEPCT24HOUR}</span></p>
+              <p>Última Actualización: <span>{result.LASTUPDATE}</span></p>
             </div>
           </div>
         </>
